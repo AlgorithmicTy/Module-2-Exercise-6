ac9211285fb207740a1365bf744d313bf5c466f0
AlgorithmicTy <31456137-AlgorithmicTy@users.noreply.replit.com>
2024-02-13 17:56:24 +0000

    Update Code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,20 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
+import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Ty");
-  }
 
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
+    Scanner input = new Scanner(System.in);
+
+    // Prompt the user to enter the subtotal and a gratuity rate
+    System.out.print("Enter the subtotal and a gratuity rate: ");
+    double subtotal = input.nextDouble();
+    double gratuityRate = input.nextDouble();
+
+    // Calculate Gratuity and Total
+    double gratuity = subtotal * (gratuityRate / 100);
+    double total = subtotal + gratuity;
+
+    // Display results
+    System.out.println("The gratuity is $" + gratuity + " and total is $" + total);
+  }
 }
